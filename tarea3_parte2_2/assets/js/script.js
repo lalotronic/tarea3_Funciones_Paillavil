8cbfb271a2bc4f24e7e46fac7754b34a14cb19b6
lalotronic <epail001@gmail.com>
2024-06-14 21:30:56 -0400

    2do commit

--- tarea3_parte2_2/assets/js/script.js
+++ tarea3_parte2_2/assets/js/script.js
@@ -3,12 +3,26 @@
     let color='yellow';
     const ele = document.getElementById("ele1")
     //aqui la funcion pintar ya no selecciona el directamente el elemento ele dentro de la funcion
-    //este oscuro truco no esta en la guia ni en las diapos 
-    function pintar(e){
-        e.target.style.backgroundColor = color;
+    
+    //function pintar(e){
+    //    e.target.style.backgroundColor = color;
+    //}
+    function pintar(ele,colorElegido){
+        ele.style.backgroundColor = colorElegido;
     }
     
-    ele.addEventListener("click", pintar);
+    ele.addEventListener("click", function(){ //aqui usamos el tip de funcion anomima
+
+        pintar(ele,color);
+    });
 
 
-  
+  //######### otra forma segun ayudantia puede ser ##############
+ // let color='yellow';
+ // const ele = document.getElementById("ele1")
+  //aqui la funcion pintar ya no selecciona el directamente el elemento ele dentro de la funcion
+  //este oscuro truco no esta en la guia ni en las diapos
+ // function pintar(e){
+ //     e.target.style.backgroundColor = color;
+ // }
+ // ele.addEventListener("click", pintar);
